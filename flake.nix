6c5f294124f805dd498149d8159f9cb65ddac403
Grace <svenjacobjacobs@protonmail.com>
2025-06-24 17:14:17 +0200

    Le fix de flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Build a cargo workspace";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
     crane = {
       url = "github:ipetkov/crane";
